df574f7d1edfd54f0f35d50adea6ff0751bfc0a1
arjunchangela <arjunkumarchangela20@gnu.ac.in>
2022-08-28 11:44:30 +0530

    practical1_1

---
+++ src/pr1_9.kt
@@ -0,0 +1,18 @@
+    fun findmax(a:Array<Int>){
+        var max = a[0]
+        var count = 0
+        for (i in a) {
+            println("ar[$count] = $i")
+            if (i > max) {
+                max = i
+            }
+            count++
+        }
+        println("largest number is:")
+        println(max)
+    }
+    fun main()
+    {
+        var a = arrayOf(13, 10, 1352, 52, 85, 114)
+        findmax(a)
+    }
